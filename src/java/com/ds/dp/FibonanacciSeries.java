73a83ac5db6006d62b692f839b2cc6327f7eff79
amsahoo <amsahoo5@gmail.com>
2026-09-22 16:59:27 +0530

    init commit

--- src/java/com/ds/dp/FibonanacciSeries.java
+++ src/java/com/ds/dp/FibonanacciSeries.java
@@ -4,24 +4,24 @@ public class FibonanacciSeries {
 
     public static void main(String[] args) {
 
-
-        int num=2;
-        int dp[]=new int[num+1];
-        int index=0;
-        System.out.println(printFibonanciWithDP(num,dp));
+        int num = 2;
+        int dp[] = new int[num + 1];
+        int index = 0;
+        System.out.println(printFibonanciWithDP(num, dp));
     }
 
-    private static int printFibonanciWithDP(int num,int[] dp) {
+    private static int printFibonanciWithDP(int num, int[] dp) {
 
-            if(num<=1)
-                return num;
-            else {
-                if (dp[num] != 0)
-                    return dp[num];
-
-                dp[num] = printFibonanciWithDP(num - 1, dp) + printFibonanciWithDP(num - 2, dp);
+        if (num <= 1)
+            return num;
+        else {
+            if (dp[num] != 0)
                 return dp[num];
-            }
+
+            dp[num] = printFibonanciWithDP(num - 1, dp) + printFibonanciWithDP(num - 2, dp);
+            return dp[num];
+            // return printFibonanciWithDP(num - 1, dp) + printFibonanciWithDP(num - 2, dp);
+        }
 
     }
 }
